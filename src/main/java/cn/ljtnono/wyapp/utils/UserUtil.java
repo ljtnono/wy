be016e7303f921d5cabbfc133a44d18012a7a877
ljtnono <ljtnono@163.com>
2019-08-05 23:46:39 +0800

    添加检测用户名和密码的函数

--- src/main/java/cn/ljtnono/wyapp/utils/UserUtil.java
+++ src/main/java/cn/ljtnono/wyapp/utils/UserUtil.java
@@ -1,6 +1,7 @@
 package cn.ljtnono.wyapp.utils;
 
-import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * 用户工具类
@@ -15,11 +16,21 @@ public class UserUtil {
             'a','b','c','d','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
             '1','2','3','4','5','6','7','8','9','0'
     };
+    /**
+     * 用户名检测正则表达式
+     * 4-10位字符数字组合
+     */
+    private static Pattern LOGIN_NAME_REGEX = Pattern.compile("^[a-zA-Z0-9_-]{4,10}$");
+    /**
+     * 密码检测正则表达式
+     * 密码是6-20位字母和数字的组合 .+*-_/
+     */
+    private static Pattern PASSWORD_REGEX = Pattern.compile("^[a-zA-Z0-9_-]{4,10}$");
     private UserUtil(){}
 
     /**
-     * 获取随机用户名
-     * @return 随机用户名
+     * 获取随机用户Id
+     * @return 随机用户Id
      */
     public static String getRadomUserId() {
         StringBuilder s = new StringBuilder();
@@ -29,12 +40,30 @@ public class UserUtil {
         return s.toString();
     }
 
-    public static String getRadomLoginName() {
-        StringBuilder s = new StringBuilder();
-        for (int i = 0; i < 10; i++) {
-            s.append(args[(int) (Math.random() * (args.length - 1))]);
+    /**
+     * 检查用户名是否符合格式
+     * @param loginName 需要检查的用户名
+     * @return 符合格式返回true  不符合格式返回false
+     *  TODO 需要进行测试
+     */
+
+    public static boolean validateLoginName(final String loginName) {
+        if (loginName.isEmpty()) {
+            return false;
         }
-        return s.toString();
+        return LOGIN_NAME_REGEX.matcher(loginName).matches();
     }
 
+    /**
+     * 检查密码是否符合格式
+     * @param password 需要检查的密码
+     * @return 符合格式返回true 不符合格式返回false
+     * TODO 需要进行测试
+     */
+    public static boolean validatePassword(final String password) {
+        if (password.isEmpty()) {
+            return false;
+        }
+        return PASSWORD_REGEX.matcher(password).matches();
+    }
 }
